d57c384750282c3d14c84296ace9047f29e02f00
Alex D'Andrea <alex@dandrea.io>
2019-07-17 22:26:47 +0200

    Add deps & parse cmdline args, read file

---
+++ src/analyze.ts
@@ -0,0 +1,27 @@
+import * as fs from 'fs';
+import { XmlDocument } from 'xmldoc';
+import * as yargs from 'yargs';
+import { promisify } from 'util';
+
+const readFile = promisify(fs.readFile);
+
+async function analyze(file: string) {
+  const data = await readFile(file);
+
+  const doc = new XmlDocument(data.toString());
+}
+
+yargs.usage('$0 <command>').command(
+  'analyze',
+  'Analyze and output stats from a given .gpx file',
+  (args: yargs.Argv) =>
+    args.option('file', { type: 'string', describe: 'File to read from', optional: false }),
+  (args: yargs.Arguments) => {
+    if (typeof args.file !== 'string') {
+      throw new Error('Missing arg file');
+    }
+    analyze(args.file)
+      .then((resolved: any) => console.log('Done'))
+      .catch(err => console.error(err));
+  },
+).argv;
